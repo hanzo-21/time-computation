949f26aef67756ce7da09b5202a36c4ffb902bc3
aayush rana magar <realnaramagaraayush@gmail.com>
2024-12-17 09:23:27 +0545

    intital commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        long start;
+        long end ;
+        long sum = 0;
+        long experiment = 999999999;
+
+        for(long x =1 ; x<experiment;x++){
+            start= System.nanoTime();
+            boolean operation =  2 > 3;
+            end = System.nanoTime();
+
+            sum = sum +(end- start);
+            if(x % 99999999 == 0 ){
+                System.out.println("point = " + x);
+                System.out.println("tottal = " + sum);
+                System.out.println("average = " + sum/x);
+            }
+        }
+        System.out.println("tottal = " + sum);
+        System.out.println("average = " + sum/experiment);
+
+
+    }
+}
